f1e18fd6a032ea8cf0beb3dda21f19805424ed22
Tomáš Jelínek <tomas@jelinek.pm>
2025-04-22 16:04:13 +0200

    Switch to .gitignore whitelist

---
+++ home/tomas/.config/fish/functions/ls.fish
@@ -0,0 +1,4 @@
+function ls --wraps='eza --group-directories-first --icons=auto' --description 'alias ls=eza --group-directories-first --icons=auto'
+  eza --group-directories-first --icons=auto $argv
+        
+end
