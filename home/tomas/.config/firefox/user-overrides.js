6fad31acd44db09c1b3053bfb7482c00ac8bc56f
Tomáš Jelínek <tomas@jelinek.pm>
2025-12-26 16:07:06 +0100

    bump

---
+++ home/tomas/.config/firefox/user-overrides.js
@@ -0,0 +1,34 @@
+// arkenfox overrides
+user_pref("browser.download.folderList", 1);
+user_pref("browser.eme.ui.enabled", true);
+user_pref("browser.startup.page", 3);
+user_pref("browser.urlbar.recentsearches.featureGate", false);
+user_pref("browser.urlbar.suggest.topsites", false);
+user_pref("dom.security.https_only_mode_pbm", true);
+user_pref("dom.security.https_only_mode_send_http_background_request", true);
+user_pref("extensions.formautofill.addresses.enabled", false);
+user_pref("extensions.formautofill.creditCards.enabled", false);
+user_pref("keyword.enabled", false);
+user_pref("media.eme.enabled", true);
+user_pref("network.trr.custom_uri", "https://dns.adguard-dns.com/dns-query");
+user_pref("network.trr.mode", 3);
+user_pref("network.trr.uri", "https://dns.adguard-dns.com/dns-query");
+user_pref("privacy.clearHistory.historyFormDataAndDownloads", true);
+user_pref("privacy.clearOnShutdown_v2.downloads", true);
+user_pref("privacy.clearOnShutdown_v2.historyFormDataAndDownloads", true);
+user_pref("security.nocertdb", true);
+user_pref("signon.rememberSignons", false);
+
+// some other stuff I like
+user_pref("browser.compactmode.show", true);
+user_pref("browser.tabs.groups.smart.userEnabled", false);
+user_pref("browser.tabs.warnOnClose", true);
+user_pref("browser.uidensity", 1);
+user_pref("browser.urlbar.placeholderName", "DuckDuckGo");
+user_pref("browser.urlbar.placeholderName.private", "DuckDuckGo");
+user_pref("identity.fxaccounts.enabled", false);
+user_pref("media.videocontrols.picture-in-picture.video-toggle.enabled", false);
+user_pref("sidebar.verticalTabs", true);
+user_pref("sidebar.visibility", "expand-on-hover");
+user_pref("signon.management.page.breach-alerts.enabled", false);
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
